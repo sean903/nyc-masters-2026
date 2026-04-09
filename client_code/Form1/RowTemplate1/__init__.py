5426cf8a90964c0fb9100e8d81ba0a9e81d1a094
Sean McNamara <sean123mcnamra@gmail.com>
2026-04-08 22:48:04 -0400

    fix scrape

--- client_code/Form1/RowTemplate1/__init__.py
+++ client_code/Form1/RowTemplate1/__init__.py
@@ -3,35 +3,6 @@ from anvil import *
 import anvil.server
 
 
-def format_score(x):
-  if x is None:
-    return ""
-  if x == 999 or x == 999.0:
-    return "N/A"
-  return f"{x:+}"
-
-
 class RowTemplate1(RowTemplate1Template):
   def __init__(self, **properties):
     self.init_components(**properties)
-
-    self.person.text = self.item["person"]
-    self.avg_score.text = f'{self.item["avg_score"]:+.2f}'
-
-    self.player_1.text = self.item["player_1"]
-    self.score_1.text = format_score(self.item["score_1"])
-
-    self.player_2.text = self.item["player_2"]
-    self.score_2.text = format_score(self.item["score_2"])
-
-    self.player_3.text = self.item["player_3"]
-    self.score_3.text = format_score(self.item["score_3"])
-
-    self.player_4.text = self.item["player_4"]
-    self.score_4.text = format_score(self.item["score_4"])
-
-    self.player_5.text = self.item["player_5"]
-    self.score_5.text = format_score(self.item["score_5"])
-
-    self.player_6.text = self.item["player_6"]
-    self.score_6.text = format_score(self.item["score_6"])
