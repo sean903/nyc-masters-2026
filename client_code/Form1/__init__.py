eb7dbf1d14490beea3c8a3ffd1513cc24e0af4ea
sean123mcnamara@gmail.com <sean123mcnamara@gmail.com>
2026-04-09 00:40:31 +0000

    Created custom package dependencies; Edited Form1.RowTemplate1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,36 +1,15 @@
-from ._anvil_designer import ItemTemplate1Template
+from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+# import anvil.users
+import anvil.server
 
 
-def format_score(x):
-  if x is None:
-    return ""
-  if x == 999 or x == 999.0:
-    return "N/A"
-  return f"{x:+}"
-
-
-class ItemTemplate1(ItemTemplate1Template):
+class Form1(Form1Template):
   def __init__(self, **properties):
     self.init_components(**properties)
 
-    self.person.text = self.item["person"]
-    self.avg_score.text = f'{self.item["avg_score"]:+.2f}'
-
-    self.player_1.text = self.item["player_1"]
-    self.score_1.text = format_score(self.item["score_1"])
-
-    self.player_2.text = self.item["player_2"]
-    self.score_2.text = format_score(self.item["score_2"])
-
-    self.player_3.text = self.item["player_3"]
-    self.score_3.text = format_score(self.item["score_3"])
-
-    self.player_4.text = self.item["player_4"]
-    self.score_4.text = format_score(self.item["score_4"])
-
-    self.player_5.text = self.item["player_5"]
-    self.score_5.text = format_score(self.item["score_5"])
-
-    self.player_6.text = self.item["player_6"]
-    self.score_6.text = format_score(self.item["score_6"])
+    data = anvil.server.call('get_person_leaderboard')
+    self.repeating_panel_1.items = data
